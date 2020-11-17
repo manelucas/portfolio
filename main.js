22528fae583c84cffa9fcc7e53e9048db9475f0e
Manuel Lucas <manuel.lucas@Nology.local>
2020-11-17 23:23:56 +0000

    restyled projects display and iits functionality

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-const scrollButton = document.querySelector("#scrollButton");
+var projectIndex = 1;
+showProjects(projectIndex);
 
+// Next/previous controls
+const plusProjects = (n) => {
+  showProjects(projectIndex += n);
+}
+
+// Thumbnail image controls
+const currentProject = (n) => {
+  showProjects(projectIndex = n);
+}
+
+function showProjects(n) {
+  let projects = document.querySelectorAll(".myProjects");
+  let dots = document.querySelectorAll(".dot");
+  if (n > projects.length) {projectIndex = 1}
+  if (n < 1) {projectIndex = projects.length}
+  for (let i = 0; i < projects.length; i++) {
+    projects[i].style.display = "none";
+  }
+  for (let j = 0; j < dots.length; j++) {
+      dots[j].className = dots[j].className.replace(" active", "");
+  }
+  projects[projectIndex-1].style.display = "flex";
+  dots[projectIndex-1].className += " active";
+}
+
+
+// scrollButton
+const scrollButton = document.querySelector("#scrollButton");
 
 scrollButton.addEventListener("click", () => {
     window.scrollTo({
