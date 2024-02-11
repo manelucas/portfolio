05194a2dd76888925178775d4361267a879b24e5
manelucas <manelglucas@gmail.com>
2024-02-11 21:40:38 +0000

    styled navbar links

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import Navbar from './components/Navbar/Navbar'
+
 
 const App = () => {
   return (
     <>
       <main className="container">
+        <Navbar />
         <h1>Welcome to my website</h1>
       </main> 
     </>
