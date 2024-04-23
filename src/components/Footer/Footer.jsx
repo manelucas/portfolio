1147c31f1e7c47e0dbccb87416f0e1dd74a37e3b
manelucas <manelglucas@gmail.com>
2024-04-24 00:38:44 +0100

    theme color adjusments

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => {
                             <span className='bold fs-5'>Manuel Lucas</span>
                             <div className='d-flex g-1'>
                                 <a href="https://www.linkedin.com/in/manuel-lucas-821b091a2/" target="_blank" rel="noreferrer" className="text-primary fs-4 text-decoration-none">
-                                    <i className="fab fa-linkedin-in fs-1 text-white"></i>
+                                    <i className="fab fa-linkedin-in fs-1 text-white" title='My Linkedin'></i>
                                 </a>
                                 <a href="https://github.com/manelucas" target="_blank" rel="noreferrer" className="text-primary fs-4 text-decoration-none">
-                                    <i className="fab fa-github fs-1 text-white"></i>  
+                                    <i className="fab fa-github fs-1 text-white" title='My Github'></i>  
                                 </a>
                             </div>
                         </div>
